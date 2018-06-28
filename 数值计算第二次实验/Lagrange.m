723ea0c27271d5131fd08a5c3c07363f1b334bfb
leungyukshing <jacky14.liang@gmail.com>
2018-06-29 01:24:56 +0800

    modify file name

--- 数值计算第二次实验/Lagrange.m
+++ 数值计算第二次实验/Lagrange.m
@@ -8,10 +8,11 @@ l = ones(1,n); % 基函数集合
 for k = 1:n
     for j = 1:n
         if j ~= k
-            l(k) = l(k)*(x0-x(j))/(x(k) - x(j));
+            l(k) = l(k)*(x0-x(j))/(x(k) - x(j)); % 计算插值基函数
         end
     end
 end
+% 计算插值多项式
 for i = 1:n
     y0 = y0 + y(i)*l(i);
 end
