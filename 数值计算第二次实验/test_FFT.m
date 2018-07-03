fea2ed6f97ac9201701ef6db2be7518672dfb916
leungyukshing <jacky14.liang@gmail.com>
2018-07-04 01:32:22 +0800

    add fft photo

--- 数值计算第二次实验/test_FFT.m
+++ 数值计算第二次实验/test_FFT.m
@@ -1,23 +1,17 @@
-t = zeros(1024,1);
-S = zeros(1024,1);
+% 测试快速傅里叶变换
+
+S = zeros(1024,1); % 采样点
 for i = 1:length(t)
-    t(i) = 2 * pi * (i-1) / 1024;
     S(i) = FFTSignalFunction(i-1);
 end
 
 c = FFT(S);
 
-T = zeros(1024,1);
-for i = 0:length(t)-1
-    T(i+1) = real(c(1))/2;
-    for k = 1:length(t)-2
-        T(i+1) = T(i+1) + real(c(k+1))*cos(k * t(i+1)) + imag(c(k+1))*sin(k * t(i+1));
-    end
-    T(i+1) = T(i+1) + real(c(length(c))) * cos(length(c) * t(i+1));
-end
-
+c_standard = fft(S);
 figure(1)
-plot(t,S);
-figure(2)
-plot(t,T);
+plot(abs(c));
+title('自己写的FFT');
 
+figure(2)
+plot(abs(c_standard));
+title('标准FFT');
