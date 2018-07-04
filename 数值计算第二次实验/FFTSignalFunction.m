fa6507cfab9f709ed31eb523ee055593f5011541
leungyukshing <jacky14.liang@gmail.com>
2018-07-04 21:31:10 +0800

    update FFT photo

--- 数值计算第二次实验/FFTSignalFunction.m
+++ 数值计算第二次实验/FFTSignalFunction.m
@@ -1,3 +1,3 @@
 function [ S ] = FFTSignalFunction( t )
-    S = 0.7*sin(2*pi*50*t) + sin(2*pi*120*t);
+    S = 0.7*sin(2*pi*50*t) + 0.2*sin(2*pi*120*t);
 end
--- 数值计算第二次实验/test_FFT.m
+++ 数值计算第二次实验/test_FFT.m
@@ -1,13 +1,14 @@
 % 测试快速傅里叶变换
 
 S = zeros(1024,1); % 采样点
-for i = 1:length(t)
+for i = 1:length(S)
     S(i) = FFTSignalFunction(i-1);
 end
 
-c = FFT(S);
+c = FFT(S); 
 
-c_standard = fft(S);
+c_standard = fft(S);% 标准
+
 figure(1)
 plot(abs(c));
 title('自己写的FFT');
