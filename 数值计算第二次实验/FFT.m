fdcca7fe553b86e04c643d5c6c7f2c1b2d2e67e8
leungyukshing <jacky14.liang@gmail.com>
2018-07-05 00:40:41 +0800

    update two FFT photo

--- 数值计算第二次实验/FFT.m
+++ 数值计算第二次实验/FFT.m
@@ -1,16 +1,15 @@
 function [c] = FFT(A)
-    N = 1024;
-    p = 10;
+    N = 1024; % 采样点数量
+    p = 10; 
     W = exp(i*2*pi/N);
-    %A = rand(1,1024);
 
     for q=1:1:p
-        B = A; % A(q-1)
+        T = A; % A(q-1)
         for k=0:1:2^(p-q)-1
             for j=0:1:2^(q-1)-1
                 index1 = k*2^(q-1);
-                temp1 = B(k*2^(q-1) + j + 1); % 运算前的A
-                temp2 = B(k*2^(q-1) + j + 2^(p-1) + 1);
+                temp1 = T(k*2^(q-1) + j + 1); % 运算前的A
+                temp2 = T(k*2^(q-1) + j + 2^(p-1) + 1);
                 A(k*2^q + j + 1) = temp1 + temp2;
                 A(k*2^q + j + 2^(q-1) + 1) = (temp1 - temp2) * (W^index1);
             end
